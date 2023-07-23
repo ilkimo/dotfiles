e3978c235820e7696a3a78e608e4cfa65efcb4f0
il_kimo <vibertikim@yahoo.it>
2023-07-23 06:52:22 +0200

    feat: add polybar

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# THIS SCRIPT ASSUMES THAT THE REPOSITORY IS CLONED AT PATH ~/repos/
+# THIS SCRIPT ASSUMES THAT THE REPOSITORY IS CLONED AT PATH ~/repos/ RESULTING IN PATH ~/repos/dotfiles
 
 # i3
 mkdir -p ~/.config/i3
@@ -12,4 +12,9 @@ mkdir -p ~/.config/picom
 rm -f ~/.config/picom/picom.conf
 ln -s ~/repos/dotfiles/picom/picom.conf ~/.config/picom/picom.conf
 
-
+# polybar
+mkdir -p ~/.config/polybar
+#ln -s ~/repos/dotfiles/polybar/config.ini ~/.config/polybar/config.ini
+#ln -s ~/repos/dotfiles/polybar/launch.sh ~/.config/polybar/launch.sh
+sudo pacman -S calc rofi
+yay -S pywal
