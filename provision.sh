a49b03cba5781575485ae7e6ee384061f45a3360
il_kimo <vibertikim@yahoo.it>
2023-12-17 00:32:32 +0100

    feat: add Neovim provisioning to install script

--- provision.sh
+++ provision.sh
@@ -149,7 +149,7 @@ fi
 echo "yay package is located at $(which yay)"
 
 echo "Installing other packages"
-install_packages sl cmatrix cowsay lolcat cava xorg-server nvidia nvidia-utils nvidia-settings xorg-xrandr xorg-xcalc neovim kitty ranger zathura feh tree vim lightdm lightdm-slick-greeter i3-wm rofi pcmanfm pywal polybar docker maim picom pavucontrol thunderbird bitwarden spotify-launcher telegram-desktop google-chrome imagemagick openlens-bin gitkraken code kubectl
+install_packages sl cmatrix cowsay lolcat cava xorg-server nvidia nvidia-utils nvidia-settings xorg-xrandr xorg-xcalc vim neovim kitty ranger zathura feh tree lightdm lightdm-slick-greeter i3-wm rofi pcmanfm pywal polybar docker maim picom pavucontrol thunderbird bitwarden spotify-launcher telegram-desktop google-chrome imagemagick openlens-bin gitkraken code kubectl
 # TODO morc_menu bmenu
 # imagemagick is for image generation (directory template_images)
 
@@ -242,6 +242,9 @@ fi
 mkdir -p ~/.config/kitty
 ln -sf $PROJECT_PATH/kitty/kitty.conf ~/.config/kitty/kitty.conf
 sudo chown --recursive "$USER:$USER" ~/.config/kitty
+
+# Setup Neovim links
+ln -sf $PROJECT_PATH/kickstart.nvim ~/.config/nvim
 
 # Set default applications
 sudo -u $USER xdg-mime default google-chrome.desktop x-scheme-handler/http
