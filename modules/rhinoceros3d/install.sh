ba8d1b989927e78cfe9683de0e10dabd7548ca46
il_kimo <vibertikim@yahoo.it>
2024-03-24 21:51:47 +0100

    chore: start adding info for wine dependencies

---
+++ modules/rhinoceros3d/install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "Yo! I am the rhinoceros3d module's install.sh, and I received the path for the cloned repo as $1"
+echo "TODOOOO this is still to do"
+echo "you have to enable multilib in /etc/pacman.conf to allow 32 bit applications, then wine will be visible in pacman to install"
