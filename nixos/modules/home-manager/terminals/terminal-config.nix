1e4c59a881822382e405b2007207683ba0906a63
il_kimo <vibertikim@yahoo.it>
2024-09-08 01:07:20 +0200

    test(NixOS): enable kitty

--- nixos/modules/home-manager/terminals/terminal-config.nix
+++ nixos/modules/home-manager/terminals/terminal-config.nix
@@ -1,13 +1,13 @@
 { terminal, lib, ... }:
 
 {
-  programs = lib.mkIf (terminal == "default" || terminal == "kitty") {
-    home.file.".config/kitty" = {
+  home = lib.mkIf (terminal == "default" || terminal == "kitty") {
+    file.".config/kitty" = {
       source = ./dotfiles/kitty;
       recursive = true;
     };
   } // lib.mkIf (terminal == "alacritty") {
-    home.file.".config/alacritty" = {
+    file.".config/alacritty" = {
       source = ./dotfiles/alacritty;
       recursive = true;
     };
