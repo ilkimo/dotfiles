b9fd83190193ff7561c7aab0c4f3c9a92681c36b
il_kimo <vibertikim@yahoo.it>
2024-09-09 22:46:42 +0200

    chore(NixOS): set coherent hostnames

--- nixos/hosts/old-laptop-uni/configuration.nix
+++ nixos/hosts/old-laptop-uni/configuration.nix
@@ -50,7 +50,7 @@ in
     };
   };
 
-   networking.hostName = "nixos"; # Define your hostname.
+   networking.hostName = "old-laptop-uni"; # Define your hostname.
   # Pick only one of the below networking options.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
    networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
