b093e430d64eea8246243dbe21099a80f14b4a64
il_kimo <vibertikim@yahoo.it>
2024-11-04 11:37:46 +0100

    chore: add tmux stuff

--- scripts/tmux-sessionizer.sh
+++ scripts/tmux-sessionizer.sh
@@ -3,7 +3,7 @@
 if [[ $# -eq 1 ]]; then
     selected=$1
 else
-    selected=$(find ~ ~/repos ~/repos/work ~/repos/work/bando_uni ~/repos/work/bando_uni/utility ~/repos/dotfiles ~/repos/projects ~/repos/projects/scuba_dive/ ~/repos/projects/life_management/ -mindepth 1 -maxdepth 1 -type d | fzf)
+    selected=$(find ~ ~/repos ~/repos/work ~/repos/work/bando_uni ~/repos/work/bando_uni/utility ~/repos/dotfiles ~/repos/projects ~/repos/projects/scuba_dive/ ~/repos/projects/life_management/ ~/repos/projects/games/minecraft ~/repos/projects/keyboards/ ~/kimo-data/all/projects ~/repos/work/ites/ ~/kimo-data/all/study -mindepth 1 -maxdepth 1 -type d | fzf)
 fi
 
 if [[ -z $selected ]]; then
