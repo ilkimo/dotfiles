497e6e1b9f6dcdea5f9ff27a9b52124032975859
il_kimo <vibertikim@yahoo.it>
2024-11-24 13:55:52 +0100

    chore(reale-ites): remove stuff

--- nixos/modules/users/work/reale-ites.nix
+++ nixos/modules/users/work/reale-ites.nix
@@ -23,24 +23,4 @@ in
       description = "reale-ites user";
     };
   };
-
-# Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.${cfg.userName} = {
-    isNormalUser = true;
-    extraGroups = ["dialout" "bluetooth" "networkmanager" ]; # To enable ‘sudo’ for the user add the 'wheel' group.
-    packages = with pkgs; [
-      google-chrome
-      vscode
-    ] ++ (lib.optionals (terminal == "kitty" || terminal == "default") [ kitty ])
-      ++ (lib.optionals (shell == "zsh" || shell == "default") [ zsh-powerlevel10k ])
-      ++ (lib.optionals (vibes == true) [ tree sl cmatrix ]);
-  };
-
-  #home-manager = {
-    # also pass inputs to home-manager modules
-  #  extraSpecialArgs = { inherit terminal; };
-  #  users = {
-  #    ${cfg.userName} = import ./home.nix;
-  #  };
-  #};
 }
