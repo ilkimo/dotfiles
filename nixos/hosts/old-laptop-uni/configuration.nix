582736a37dbf52e5503b2b61215602e3c3c67c39
il_kimo <vibertikim@yahoo.it>
2024-12-20 22:28:30 +0100

    refactor(NixOS): remove new lines

--- nixos/hosts/old-laptop-uni/configuration.nix
+++ nixos/hosts/old-laptop-uni/configuration.nix
@@ -91,4 +91,3 @@ in
   # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
   system.stateVersion = "24.05"; # Did you read the comment?
 }
-
